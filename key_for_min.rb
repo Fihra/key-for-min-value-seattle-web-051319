3758fc0341d798258c078691dc5ffc31ea3f1658
Fabian Fabro <fabrofabian@gmail.com>
2019-04-28 03:40:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,8 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
-    if name_hash[:value] <= 1
-      return name_hash[:value]
+    if value <= 1
+      return value
     end
   end
 
